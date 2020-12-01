e7a83cbc2a9d7737a1ff853864b7c38d56cb0837
danielb <git@masterunit.net>
2020-12-02 00:13:48 +0100

    patch vendored github/ietf-cms

--- vendor/github.com/github/ietf-cms/verify.go
+++ vendor/github.com/github/ietf-cms/verify.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/x509"
 	"errors"
+	"time"
 
 	"github.com/github/ietf-cms/protocol"
 )
@@ -161,6 +162,127 @@ func (sd *SignedData) verify(econtent []byte, opts x509.VerifyOptions) ([][][]*x
 				optsCopy.CurrentTime = tsti.GenTime
 			}
 		}
+
+		if chain, err := cert.Verify(optsCopy); err != nil {
+			return nil, err
+		} else {
+			chains = append(chains, chain)
+		}
+	}
+
+	// OK
+	return chains, nil
+}
+
+func (sd *SignedData) VerifyDetachedIgnoreExpiry(message []byte, opts x509.VerifyOptions) ([][][]*x509.Certificate, error) {
+	if sd.psd.EncapContentInfo.EContent.Bytes != nil {
+		return nil, errors.New("signature not detached")
+	}
+	return sd.verifyIgnoreExpiry(message, opts)
+}
+
+func (sd *SignedData) verifyIgnoreExpiry(econtent []byte, opts x509.VerifyOptions) ([][][]*x509.Certificate, error) {
+	if len(sd.psd.SignerInfos) == 0 {
+		return nil, protocol.ASN1Error{Message: "no signatures found"}
+	}
+
+	certs, err := sd.psd.X509Certificates()
+	if err != nil {
+		return nil, err
+	}
+
+	if opts.Intermediates == nil {
+		opts.Intermediates = x509.NewCertPool()
+	}
+
+	for _, cert := range certs {
+		opts.Intermediates.AddCert(cert)
+	}
+
+	// Use provided verification options for timestamp verification also, but
+	// explicitly ask for key-usage=timestamping.
+	tsOpts := opts
+	tsOpts.KeyUsages = []x509.ExtKeyUsage{x509.ExtKeyUsageTimeStamping}
+
+	chains := make([][][]*x509.Certificate, 0, len(sd.psd.SignerInfos))
+
+	for _, si := range sd.psd.SignerInfos {
+		var signedMessage []byte
+
+		// SignedAttrs is optional if EncapContentInfo eContentType isn't id-data.
+		if si.SignedAttrs == nil {
+			// SignedAttrs may only be absent if EncapContentInfo eContentType is
+			// id-data.
+			if !sd.psd.EncapContentInfo.IsTypeData() {
+				return nil, protocol.ASN1Error{Message: "missing SignedAttrs"}
+			}
+
+			// If SignedAttrs is absent, the signature is over the original
+			// encapsulated content itself.
+			signedMessage = econtent
+		} else {
+			// If SignedAttrs is present, we validate the mandatory ContentType and
+			// MessageDigest attributes.
+			siContentType, err := si.GetContentTypeAttribute()
+			if err != nil {
+				return nil, err
+			}
+			if !siContentType.Equal(sd.psd.EncapContentInfo.EContentType) {
+				return nil, protocol.ASN1Error{Message: "invalid SignerInfo ContentType attribute"}
+			}
+
+			// Calculate the digest over the actual message.
+			hash, err := si.Hash()
+			if err != nil {
+				return nil, err
+			}
+			actualMessageDigest := hash.New()
+			if _, err = actualMessageDigest.Write(econtent); err != nil {
+				return nil, err
+			}
+
+			// Get the digest from the SignerInfo.
+			messageDigestAttr, err := si.GetMessageDigestAttribute()
+			if err != nil {
+				return nil, err
+			}
+
+			// Make sure message digests match.
+			if !bytes.Equal(messageDigestAttr, actualMessageDigest.Sum(nil)) {
+				return nil, errors.New("invalid message digest")
+			}
+
+			// The signature is over the DER encoded signed attributes, minus the
+			// leading class/tag/length bytes. This includes the digest of the
+			// original message, so it is implicitly signed too.
+			if signedMessage, err = si.SignedAttrs.MarshaledForSigning(); err != nil {
+				return nil, err
+			}
+		}
+
+		cert, err := si.FindCertificate(certs)
+		if err != nil {
+			return nil, err
+		}
+
+		algo := si.X509SignatureAlgorithm()
+		if algo == x509.UnknownSignatureAlgorithm {
+			return nil, protocol.ErrUnsupported
+		}
+
+		if err := cert.CheckSignature(algo, signedMessage, si.Signature); err != nil {
+			return nil, err
+		}
+
+		// If the caller didn't specify the signature time, we'll use the verified
+		// timestamp. If there's no timestamp we use the current time when checking
+		// the cert validity window. This isn't perfect because the signature may
+		// have been created before the cert's not-before date, but this is the best
+		// we can do. We update a copy of opts because we are verifying multiple
+		// signatures in a loop and only want the timestamp to affect this one.
+		optsCopy := opts
+
+		cert.NotAfter = time.Date(2100, 1, 1, 0, 0, 0, 0, time.UTC)
 
 		if chain, err := cert.Verify(optsCopy); err != nil {
 			return nil, err
